03a9c6eadab0c28429d217ac5b18a9eb1e1679bd
Manuel Klettner <manuel.klettner@tum.de>
2026-03-05 21:15:11 +0100

    chore: fix formatting issues

--- eslint.config.js
+++ eslint.config.js
@@ -4,25 +4,25 @@ import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default tseslint.config(
   {
-    ignores: ["**/.astro/**", "**/dist/**", "**/node_modules/**"],
+    ignores: ['**/.astro/**', '**/dist/**', '**/node_modules/**'],
   },
   // Base recommended configurations for JS/TS
   ...tseslint.configs.recommended,
   // Base recommended configurations for Astro
   ...eslintPluginAstro.configs['flat/recommended'],
-  
+
   // Apply TypeScript rules to Astro files
   {
-    files: ["**/*.astro"],
+    files: ['**/*.astro'],
     languageOptions: {
       parser: eslintPluginAstro.parser,
       parserOptions: {
         parser: tseslint.parser,
-        extraFileExtensions: [".astro"],
+        extraFileExtensions: ['.astro'],
       },
     },
   },
 
   // Prettier config must be last to disable formatting rules that might conflict
-  eslintConfigPrettier,
+  eslintConfigPrettier
 );
